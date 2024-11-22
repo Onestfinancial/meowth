cf07f83942db80383e7af04d3ab583cb1e584720
josecg20 <cazorla@onestfinancial.com>
2024-11-22 10:20:38 -0600

    Update config files

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,4 @@
 export const environmentConfig = {
-  environment: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3001,
+  environment: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3012,
 };
--- src/config/server.ts
+++ src/config/server.ts
@@ -1,10 +1,9 @@
-// src/config/server.ts
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const serverConfig = {
-  // rewardServiceUrl:
-  //   process.env.CUSTOMER_SERVICE_URL || 'http://localhost:3001/api',
-  // Add other service URLs or configuration settings here
+  customerServiceUrl:
+    process.env.CUSTOMER_SERVICE_URL || "http://localhost:3001/api",
+  //Add other service URLs or configuration settings here
 };
